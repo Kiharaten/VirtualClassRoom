1dec3a2f05148497fd3a23e3991006500d8e22b2
Kiharaten <kiharaten1129@gmail.com>
2020-11-11 11:06:27 +0900

    replace http response method

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@
 
 # Create your views here.
 
-from django.http import HttpResponse, Http404
+# from django.http import HttpResponse, Http404
 # from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
@@ -33,8 +33,12 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+    response = "You're looking at the results of question %s." % question_id
+    return render(request, 'polls/response.html', {'response': response})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+    response = "You're voting on question %s." % question_id
+    return render(request, 'polls/response.html', {'response': response})
