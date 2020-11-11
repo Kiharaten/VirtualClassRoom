4a4b92c992bf99ebec4c2811f58830ecb7529ff1
Kiharaten <kiharaten1129@gmail.com>
2020-11-11 12:32:24 +0900

    add app of kiharax component

--- kiharax/urls.py
+++ kiharax/urls.py
@@ -4,4 +4,19 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('name', views.name, name='name'),
+    path('gender', views.gender, name='gender'),
+    path('age', views.age, name='age'),
+    path('height', views.height, name='height'),
+    path('weight', views.weight, name='weight'),
+    path('description', views.description, name='description'),
+
+    # ex:
+    # index       -> /kiharax/
+    # name        -> /kiharax/name/
+    # gender      -> /kiharax/gender/
+    # age         -> /kiharax/age/
+    # height      -> /kiharax/height/
+    # weight      -> /kiharax/weight/
+    # description -> /kiharax/description/
 ]
--- kiharax/views.py
+++ kiharax/views.py
@@ -9,7 +9,46 @@
 
 # Create your views here.
 
-from django.http import HttpResponse
+# ex:
+    # index       -> /kiharax/
+    # name        -> /kiharax/name/
+    # gender      -> /kiharax/gender/
+    # age         -> /kiharax/age/
+    # height      -> /kiharax/height/
+    # weight      -> /kiharax/weight/
+    # description -> /kiharax/description/
+
+from django.http import HttpResponse, Http404
+# from django.shortcuts import get_object_or_404, render
+profile = {
+    "name" : "Hi, my name is Sora Kihara.",
+    "gender" : "Yeah, my gender is men.",
+    "age" : "Ok, my age is 20 years old.",
+    "height" : "No problem, my height is about 170cm.",
+    "weight" : "sure, my weight is about 55kg.",
+}
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the kiharax index.")
+
+def name(request):
+    return HttpResponse(profile["name"])
+
+def gender(request):
+    return HttpResponse(profile["gender"])
+
+def age(request):
+    return HttpResponse(profile["age"])
+
+def height(request):
+    return HttpResponse(profile["height"])
+
+def weight(request):
+    return HttpResponse(profile["weight"])
+
+def description(request):
+    description = ""
+    for value in profile.values():
+        description = description + value + " "
+    return HttpResponse(description)
