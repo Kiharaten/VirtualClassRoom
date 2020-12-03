4f4f021e6449889c415208fedd48a8dd6334d1f1
Sora Kihara <sorakihara@SoranoiMac.local>
2020-12-03 17:16:11 +0900

    テンプレート追加

--- VirtualClassRoom/views.py
+++ VirtualClassRoom/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+# from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
 def index(request):
     response = "view index data"
-    return render(request, 'index.index.html', {'response' : response})
+    # return render(request, 'VirtualClassRoom/index.html', {'response' : response})
+    return HttpResponse(response)
 
