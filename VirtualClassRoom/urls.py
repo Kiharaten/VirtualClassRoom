bc7d630e31de4e4067aab7a5b1c45f02657e1658
Kiharaten <kiharaten1129@gmail.com>
2020-12-08 10:28:58 +0900

    create templates indexm, login

--- VirtualClassRoom/urls.py
+++ VirtualClassRoom/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'VirtualClassRoom'
+
 urlpatterns = [
     path('',views.index, name='index'),
+    path('login', views.login, name='login'),
 ]
--- VirtualClassRoom/views.py
+++ VirtualClassRoom/views.py
@@ -1,8 +1,13 @@
-# from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import User
+
 def index(request):
-    response = "view index data"
-    # return render(request, 'VirtualClassRoom/index.html', {'response' : response})
+    # response = "view index data"
+    return render(request, 'VirtualClassRoom/index.html', {'response' : response})
     return HttpResponse(response)
 
+def login(request):
+    # response = "login page"
+    return render(request, 'VirtualClassRoom/login.html', {'response' : response})
