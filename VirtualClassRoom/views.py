eb2ff2ae31407c174fd4f0e3f08cf64c7133838e
Kiharaten <kiharaten1129@gmail.com>
2020-12-08 12:48:10 +0900

    add mypage

--- VirtualClassRoom/views.py
+++ VirtualClassRoom/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import User
+# from .models import User
+
+response = {
+    "title" : "",
+    "h1" : "",
+}
 
 def index(request):
-    # response = "view index data"
+    response["title"] = "VirtualClassRoom | welcome"
+    response["h1"] = "view page index"
+    
     return render(request, 'VirtualClassRoom/index.html', {'response' : response})
-    return HttpResponse(response)
 
 def login(request):
-    # response = "login page"
+    response = "login page"
     return render(request, 'VirtualClassRoom/login.html', {'response' : response})
+
+def mypage(request):      # removed user_id
+    response = "test_user1"
+    return render(request, 'VirtualClassRoom/mypage.html', {'response' : response})
+
+def mypage2(request):      # removed user_id
+    response = "test_user2"
+    return render(request, 'VirtualClassRoom/mypage.html', {'response' : response})
+
+def mypage3(request):      # removed user_id
+    response = "test_user3"
+    return render(request, 'VirtualClassRoom/mypage.html', {'response' : response})
+
+def mypage4(request):      # removed user_id
+    response = "form_user1"
+    return render(request, 'VirtualClassRoom/mypage_form.html', {'response' : response})
+
+def mypage5(request):      # removed user_id
+    response = "form_user2"
+    return render(request, 'VirtualClassRoom/mypage_form.html', {'response' : response})
+
+def mypage6(request):      # removed user_id
+    response = "form_user3"
+    return render(request, 'VirtualClassRoom/mypage_form.html', {'response' : response})
+
+ 
