62872c90ca9ad8dbe0a0109cefae4a12136c5173
Kiharaten <kiharaten1129@gmail.com>
2021-02-02 16:44:00 +0900

    add migrate rule

--- configure/models.py
+++ configure/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    gender = models.IntegerField(default=0)
+    birthday = models.DateField('date birthday')
+    profile = models.models.CharField(max_length=255)
+
+class ClassRoom(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.models.CharField(max_length=255)
+    subject = models.models.IntegerField(default=0)
+    start = models.models.DateTimeField(auto_now=False, auto_now_add=False)
+    length = models.models.IntegerField(default=30)
+
+
+
--- student/urls.py
+++ student/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # ex: /student/
-    path('/join/', views.join, name='join'), # ex: /student/5/join/
+    path('<int:student_id>/join/', views.join, name='join'), # ex: /student/5/join/
     path('<int:student_id>/archive/', views.archive, name='archive'), # ex: /student/5/archive/
     path('<int:student_id>/mypage/', views.mypage, name='mypage'), # ex: /student/5/mypage/
 ]
