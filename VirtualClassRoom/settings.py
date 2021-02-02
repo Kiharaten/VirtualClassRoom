fd2c09461aa29e150f7640e7f0723ea789fbbee3
Kiharaten <kiharaten1129@gmail.com>
2021-02-02 17:04:54 +0900

    add parent, configure for setting.py

--- VirtualClassRoom/settings.py
+++ VirtualClassRoom/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'student.apps.StudentConfig',
+    'parent.apps.ParentConfig',
+    'configure.apps.ConfigureConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- configure/models.py
+++ configure/models.py
@@ -7,14 +7,14 @@ class Teacher(models.Model):
     password = models.CharField(max_length=20)
     gender = models.IntegerField(default=0)
     birthday = models.DateField('date birthday')
-    profile = models.models.CharField(max_length=255)
+    profile = models.CharField(max_length=255)
 
 class ClassRoom(models.Model):
     title = models.CharField(max_length=50)
-    description = models.models.CharField(max_length=255)
-    subject = models.models.IntegerField(default=0)
-    start = models.models.DateTimeField(auto_now=False, auto_now_add=False)
-    length = models.models.IntegerField(default=30)
+    description = models.CharField(max_length=255)
+    subject = models.IntegerField(default=0)
+    start = models.DateTimeField(auto_now=False, auto_now_add=False)
+    length = models.IntegerField(default=30)
 
 
 
