1fd4afbac4a7c220401688ccd6f0797695c59786
Sora Kihara <sorakihara@SoranoiMac.local>
2021-02-04 01:29:34 +0900

    ligin add UI

--- parent/views.py
+++ parent/views.py
@@ -3,32 +3,38 @@ from django.shortcuts import get_object_or_404, render
 context = {
     'fixed': {
         'sitename': '遠隔授業システム',
-        'title': '',
+        'title': '- 保護者ログイン -',
+    },
+    'login':{
+        'username':'ユーザー名を入力',
+        'password':'パスワードを入力',
+        'login':'ログイン',
+        'student':'生徒ログイン',
+        'parent':'保護者ログイン',
+        'help':'ログインできない場合',
     },
     'nav':{
         'payment':'お支払い情報',
         'status':'学習状況',
         'notice':'お知らせ',
-    }
+    },
 }
 
 # Create your views here.
 def index(request):
-    context["fixed"]["title"] = "login"
     return render(request, 'parent/login.html', context)
-    # return HttpResponse("you can login from here.")
+
+def help(request):
+    return render(request, 'parent/login.html', context)
 
 def payment(request):
     context["fixed"]["title"] = context["nav"]["payment"]
     return render(request, 'parent/payment.html', context)
-    # return HttpResponse("you can join classroom from here.")
 
 def status(request):
     context["fixed"]["title"] = context["nav"]["status"]
     return render(request, 'parent/status.html', context)
-    # return HttpResponse("you can see archive from here.")
 
 def notice(request):
     context["fixed"]["title"] = context["nav"]["notice"]
     return render(request, 'parent/notice.html', context)
-    # return HttpResponse("you can check mypage here.")
--- student/views.py
+++ student/views.py
@@ -3,32 +3,45 @@ from django.shortcuts import get_object_or_404, render
 context = {
     'fixed': {
         'sitename': '遠隔授業システム',
-        'title': '',
+        'title': '- 生徒ログイン -',
+    },
+    'login':{
+        'username':'ユーザー名を入力',
+        'password':'パスワードを入力',
+        'login':'ログイン',
+        'student':'生徒ログイン',
+        'parent':'保護者ログイン',
+        'help':'ログインできない場合',
     },
     'nav':{
         'join':'講義に参加',
         'archive':'ビデオ視聴',
         'mypage':'マイページ',
-    }
+    },
 }
 
 # Create your views here.
+# get
 def index(request):
-    context["fixed"]["title"] = "login"
     return render(request, 'student/login.html', context)
-    # return HttpResponse("you can login from here.")
+
+def help(request):
+    return render(request, 'parent/login.html', context)
 
 def join(request):
     context["fixed"]["title"] = context["nav"]["join"]
     return render(request, 'student/join.html', context)
-    # return HttpResponse("you can join classroom from here.")
 
 def archive(request):
     context["fixed"]["title"] = context["nav"]["archive"]
     return render(request, 'student/archive.html', context)
-    # return HttpResponse("you can see archive from here.")
 
 def mypage(request):
     context["fixed"]["title"] = context["nav"]["mypage"]
     return render(request, 'student/mypage.html', context)
-    # return HttpResponse("you can check mypage here.")
+
+
+# post
+def login(request, username, password):
+    # get_object_or_404()
+    return join(request)
